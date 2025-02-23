cd66042c48a611b1d923c4508d07e783ea3b48d8
JonKanmax <daika9xjapan@gmail.com>
2025-02-23 06:58:08 -0800

    Cập nhật code và thêm hỗ trợ GitHub Pages

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function App() {
+    return ( <
+        div >
+        <
+        h1 > Web Thanh Toán - Đang triển khai! < /h1> <
+        p > Chào mừng bạn đến với trang web thử nghiệm! < /p> <
+        /div>
+    );
+}
+
+export default App;
